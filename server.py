e25fa0b427b12712e0f980b5682f7a716e606bd4
Huang Weizhuo <18071099026@163.com>
2026-04-20 20:06:59 +0800

    feat: server basic socket and multi-thread

---
+++ server.py
@@ -0,0 +1,20 @@
+import socket
+import threading
+
+def main():
+    HOST = '0.0.0.0'
+    PORT = 51234
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(5)
+    print("Server started")
+
+    while True:
+        conn, addr = server.accept()
+        threading.Thread(target=handle_client, args=(conn,)).start()
+
+def handle_client(conn):
+    conn.close()
+
+if __name__ == "__main__":
+    main()
