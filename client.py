2c616263ef1c595ae05acc35de65d6bc1363682c
Huang Weizhuo <18071099026@163.com>
2026-04-20 20:15:42 +0800

    feat: client send protocol messages

--- client.py
+++ client.py
@@ -11,3 +11,32 @@ def main():
 
 if __name__ == "__main__":
     main()
+def format_length(s):
+    return f"{len(s):03d}"
+
+def main():
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    fp = sys.argv[3]
+    s = socket.socket()
+    s.connect((host, port))
+    
+    with open(fp) as f:
+        for line in f:
+            line = line.strip()
+            parts = line.split(maxsplit=2)
+            cmd, k = parts[0], parts[1]
+            v = parts[2] if len(parts)>=3 else ''
+            
+            if cmd == 'PUT':
+                msg = f'P {k} {v}'
+            elif cmd == 'READ':
+                msg = f'R {k}'
+            elif cmd == 'GET':
+                msg = f'G {k}'
+            else:
+                continue
+            
+            full = format_length(msg) + msg
+            s.send(full.encode())
+    s.close()
